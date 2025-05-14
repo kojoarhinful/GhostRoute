3c2a1396fcaf27f9560ed9f69a481d7b0d347778
kojoarhinful <bismarkpng@gmail.com>
2025-05-14 17:58:29 +0000

    feat: add dynamic inputs and minification

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
 import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import path from 'path';
+
+
+const inputFiles = Object.fromEntries(
+  glob.sync('public/**/*.html').map((file) => [
+    path.relative('public', file).replace(/\.html$/, ''),
+    file,
+  ])
+);
 
 export default defineConfig({
   root: 'public',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    // Generate sourcemaps
+    sourcemap: process.env.NODE_ENV === 'development',
+    minify: 'esbuild',
     rollupOptions: {
-      input: {
-        main: './index.html',
-        about: './about/index.html',
-        contact: './contact/index.html'
-      }
-    }
-  }
+      input: inputFiles,
+    },
+  },
+  // add plugins
+  plugins: [
+  ],
 });
